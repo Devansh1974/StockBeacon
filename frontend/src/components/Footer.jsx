4e1da1e2e6ef069c5f5ab2533ea5024f05219c0b
Devansh singh <devanshsingh1974@gmail.com>
2025-03-11 23:08:27 +0530

    added the navbar and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from 'react'
 
-const FooterNavbar = () => {
+const Footer = () => {
   return (
-    <footer className="bg-gray-100 text-gray-600 p-4 text-center">
-      <p className="text-sm">© 2025 StockBeacon. All rights reserved.</p>
-    </footer>
-  );
-};
+        <div>
+            <div className=' flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-3 mt-40 text-sm'>
+                <div>
+                        <p className='px-10 py-3 w-full md:w-2/3 text-gray-600'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi officiis blanditiis tenetur voluptas quod possimus recusandae dolore accusantium velit eligendi!
+                        </p>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-2'>COMPANY</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>HOME</li>
+                        <li>ABOUT US</li>
+                        <li>Notice & Rules</li>
+                        <li>PRIVACY POLICY</li>
+                    </ul>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-4'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>+1 372-288-2782</li>
+                        <li>contact@stockBeacon.in</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <hr />
+                <p className='py-3 text-center text-sm'>Copyright 2025@ forever.com - All Rights Reserved. </p>
+            </div>
+        </div>
+      )
+    }
 
-export default FooterNavbar;
+export default Footer
