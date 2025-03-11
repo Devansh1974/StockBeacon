c8a4280521e4a712a34a4cd1873e5cf26df9f6f8
Devansh singh <devanshsingh1974@gmail.com>
2025-03-11 23:37:15 +0530

    initiated a backend server and connected a mongodb

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/api/test', (req, res) => res.send('Backend is running!'));
+
+const PORT = process.env.PORT || 6900;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
