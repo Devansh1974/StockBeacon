ea74878b8d710ecdb76416e4ae1bf10a82aee393
Devansh singh <devanshsingh1974@gmail.com>
2025-03-28 18:47:23 +0530

    blank signup

---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        {/* Add form fields here */}
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
